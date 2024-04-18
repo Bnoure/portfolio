29c19e4734d62ddea8cb8bf70c40cdaa5970b05f
Benkerroum Noureddine <noureddine.benkerroum@gmail.com>
2024-04-18 23:11:17 +0200

    Revert "Mise à jour des fichiers bento-grid.tsx et bento.tsx"
    This reverts commit 2eb3fa60c8c74eaf439939a62ca0279fad423116.

--- components/bento/bento-grid.tsx
+++ components/bento/bento-grid.tsx
@@ -21,52 +21,47 @@ const BentoGrid = ({
 const BentoCard = ({
 	name,
 	className,
-
+	background,
 	iconPath,
 	description,
 	href,
 	cta,
-	imgStyle, // Ajouter imgStyle dans les props
-	textStyle, // Ajouter textStyle dans les props
 }: {
 	name: string
-	className?: string
-
+	className: string
+	background: ReactNode
 	iconPath: string
 	description: string
 	href: string
 	cta: string
 	imgStyle?: { [key: string]: string } // Ajouter imgStyle dans les props
 	textStyle?: { [key: string]: string } // Ajouter textStyle dans les props
-}) => {
-	const { t } = useTranslation()
-	return (
-		<div
-			key={name}
-			className={cn(
-				'relative rounded-xl overflow- shadow-md',
-
-				'group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl',
-				'bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
-				'transform-gpu dark:bg-black dark:border-gray-300 dark:shadow-lg dark:text-black',
-				className
-			)}
-		>
-			<div className='pointer-events-none z-10 flex transform-gpu flex-col  gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10'>
-				<img
-					src={iconPath}
-					alt={name}
-					className=' origin-center transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75'
-					style={imgStyle}
-				/>
-				<h3
-					style={textStyle}
-					className='text-xl font-semibold text-neutral-700 dark:text-neutral-300'
-				>
-					{t(name)}
-				</h3>
-				<p className='max-w-lg text-neutral-400'>{t(description)}</p>
-			</div>
+}) => (
+	<div
+		key={name}
+		className={cn(
+			'group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl',
+			'bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
+			'transform-gpu dark:bg-black dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]',
+			className
+		)}
+	>
+		<div>{background}</div>
+		<div className='pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10'>
+			<img
+				src={iconPath}
+				alt={name}
+				className='h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75'
+				style={imgStyle}
+			/>
+			<h3
+				style={textStyle}
+				className='text-xl font-semibold text-neutral-700 dark:text-neutral-300'
+			>
+				{name}
+			</h3>
+			<p className='max-w-lg text-neutral-400'>{description}</p>
+		</div>
 
 			<div
 				className={cn(
