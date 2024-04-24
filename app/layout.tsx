002a9ae156afd3e1efe80c2382859b5064fe5b42
Benkerroum Noureddine <noureddine.benkerroum@gmail.com>
2024-04-24 23:34:19 +0200

    Mise à jour de l'importation du composant SpeedInsights dans le fichier _app.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from '@vercel/analytics/react'
 import Navbar from '@/components/Navbar'
 import Provider from '@/components/Provider'
 import StickySocial from '@/components/StickySocial'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import './globals.css'
 import Footer from '@/components/Footer'
@@ -17,6 +18,7 @@ const inter = Inter({
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<Provider>
+			<SpeedInsights />
 			<Navbar />
 			<div className='max-w-2xl w-[92vw] sm:w-[90vw] mx-auto pt-20'>
 				<StickySocial />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,10 @@ import '../app/globals.css'
 import RootLayout from '../app/layout'
 import nextI18NextConfig from '../next-i18next.config.js'
 import 'node_modules/flag-icons/css/flag-icons.min.css'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<RootLayout>
-			<SpeedInsights />
 			<Component {...pageProps} />
 		</RootLayout>
 	)
