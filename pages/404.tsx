999ddc562e7c83aee6a586ecad47b90ce137b6e6
noureddine benkerroum <noureddine.benkerroum@gmail.com>
2024-05-02 09:48:15 +0200

    Mise à jour des pages 404.tsx et not-found.tsx avec le composant Suspense dans React

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { useTranslation } from 'next-i18next'
 
 interface ErrorPageProps {
@@ -10,20 +10,22 @@ export default function ErrorPage({ statusCode }: ErrorPageProps) {
 	const { t } = useTranslation()
 
 	return (
-		<div className='h-[calc(100vh-400px)] mt-10'>
-			<h2 className='heading'>
-				{statusCode
-					? t('common.errorWithCode', { statusCode })
-					: t('common.errorTitle')}
-			</h2>
-			<p className='mb-10'>{t('common.oopsMessage')}</p>
-			<p>
-				{t('common.nonExistentPage')}
-				<Link href='/' legacyBehavior>
-					<a className='link'>{t('common.siteLink')}</a>
-				</Link>
-				{t('common.turnDetour')}
-			</p>
-		</div>
+		<Suspense fallback={<div>Loading navigation...</div>}>
+			<div className='h-[calc(100vh-400px)] mt-10'>
+				<h2 className='heading'>
+					{statusCode
+						? t('common.errorWithCode', { statusCode })
+						: t('common.errorTitle')}
+				</h2>
+				<p className='mb-10'>{t('common.oopsMessage')}</p>
+				<p>
+					{t('common.nonExistentPage')}
+					<Link href='/' legacyBehavior>
+						<a className='link'>{t('common.siteLink')}</a>
+					</Link>
+					{t('common.turnDetour')}
+				</p>
+			</div>
+		</Suspense>
 	)
 }
--- pages/not-found.tsx
+++ pages/not-found.tsx
@@ -1,23 +1,25 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { useTranslation } from 'next-i18next'
 
 export default function NotFound() {
 	const { t } = useTranslation()
 	return (
-		<div className='h-[calc(100vh-400px)] mt-10'>
-			<h2 className='heading'>
-				<p>{t('common.errorWithCode')} </p>
-				<p>{t('common.errorTitle')}</p>
-			</h2>
-			<p className='mb-10'>{t('common.oopsMessage')}</p>
-			<p>
-				{t('common.nonExistentPage')}
-				<Link href='/' legacyBehavior>
-					<a className='link'>{t('common.siteLink')}</a>
-				</Link>
-				{t('common.turnDetour')}
-			</p>
-		</div>
+		<Suspense fallback={<div>Loading navigation...</div>}>
+			<div className='h-[calc(100vh-400px)] mt-10'>
+				<h2 className='heading'>
+					<p>{t('common.errorWithCode')} </p>
+					<p>{t('common.errorTitle')}</p>
+				</h2>
+				<p className='mb-10'>{t('common.oopsMessage')}</p>
+				<p>
+					{t('common.nonExistentPage')}
+					<Link href='/' legacyBehavior>
+						<a className='link'>{t('common.siteLink')}</a>
+					</Link>
+					{t('common.turnDetour')}
+				</p>
+			</div>
+		</Suspense>
 	)
 }
