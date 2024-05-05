4992316c3c5e3acdbacd196283c992bd4351415a
noureddine benkerroum <noureddine.benkerroum@gmail.com>
2024-05-05 13:06:50 +0200

    Mise à jour des styles de la page [slug].tsx pour centrer le contenu et supprimer les marges supplémentaires

--- pages/projets/[slug].tsx
+++ pages/projets/[slug].tsx
@@ -43,12 +43,12 @@ const ProjectDetails: React.FC<Props> = ({ project }) => {
 				/>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<section className=' prose prose-quoteless prose-neutral dark:prose-invert mb-2 mr-2 p-2 justify-center text-center '>
+			<article className=' prose prose-quoteless prose-neutral dark:prose-invert mb-2 mr-2 p-2 justify-center text-center '>
 				<FadeDown duration={0.4}>
 					<h1 className='text-4xl font-bold text-gray-900 dark:text-white mb-6'>
 						{project.title}
 					</h1>
-					<div className='text-justify'>
+					<div className='item-center text-justify'>
 						<Mdx code={project.body.code} />
 					</div>
 					<Image
@@ -75,7 +75,7 @@ const ProjectDetails: React.FC<Props> = ({ project }) => {
 						)}
 					</div>
 				</FadeDown>
-			</section>
+			</article>
 		</>
 	)
 }
